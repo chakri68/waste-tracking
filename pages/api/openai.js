d193613d3343a354ebb81370014d75877c1ad26a
chaitanya <chaitanya.tata215@gmail.com>
2023-01-19 11:59:08 +0530

    Added fallback text to openai api

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -12,20 +12,6 @@ export default async function handler(req, res) {
     const {
       body: { location, description, date },
     } = req;
-    //     return res.status(200).json({
-    //       ok: true,
-    //       result: `
-    //   Dear [Government Authority],
-
-    // I am writing to inquire about the trash collection schedule in ${location}. I noticed that the trash along the streets has not been collected since ${date}. As a result, the area has become unsightly with piles of garbage and plastic bags thrown along the side of the road.
-
-    // I understand that there may be unforeseen circumstances that may cause delays in trash collection, but I believe it is important for the community to maintain a clean and healthy environment. I would greatly appreciate any information you can provide on when the trash in London will be collected and any steps that can be taken to prevent this from happening in the future.
-
-    // Thank you for your attention to this matter. I look forward to hearing from you soon.
-
-    // Sincerely,
-    // [Your Name]`,
-    //     });
     try {
       const mailBody = await openai.createCompletion({
         model: "text-davinci-003",
@@ -37,7 +23,16 @@ export default async function handler(req, res) {
       res.status(200).json({ ok: true, result: mailBody.data });
     } catch (err) {
       console.log("Caught Error: ", err);
-      res.status(500).json({ ok: false, result: null });
+      return res.status(200).json({
+        ok: true,
+        result: {
+          choices: [
+            {
+              text: `Dear [Government Authority], %0D%0A%0D%0AI am writing to inquire about the trash collection schedule in ${location}. I noticed that the trash along the streets has not been collected since ${date}. As a result, the area has become unsightly with piles of garbage and plastic bags thrown along the side of the road. %0D%0A%0D%0AI understand that there may be unforeseen circumstances that may cause delays in trash collection, but I believe it is important for the community to maintain a clean and healthy environment. I would greatly appreciate any information you can provide on when the trash in London will be collected and any steps that can be taken to prevent this from happening in the future. %0D%0A%0D%0AThank you for your attention to this matter. I look forward to hearing from you soon. %0D%0A%0D%0ASincerely, %0D%0A${token.user.username}`,
+            },
+          ],
+        },
+      });
     }
   } else {
     res
