9caeb74dd3c3e85c9dde2957275aac3c55554573
delta68 <94835302+delta68@users.noreply.github.com>
2023-01-20 16:23:27 +0530

    Added Report stats

--- components/Organizer/OrgReportStat.js
+++ components/Organizer/OrgReportStat.js
@@ -3,18 +3,35 @@ import { Chart } from "primereact/chart";
 import NavBar from "../NavBar";
 
 const OrgReportStats = ({ reportData }) => {
-  const [chartData] = useState({
-    labels: ["A", "B", "C"],
-    datasets: [
-      {
-        data: [300, 50, 100],
-        backgroundColor: ["#42A5F5", "#66BB6A", "#FFA726"],
-        hoverBackgroundColor: ["#64B5F6", "#81C784", "#FFB74D"],
-      },
-    ],
-  });
+  function generateStatistics(
+    data,
+    styles = {
+      backgroundColor: ["#FFA726", "#66BB6A"],
+      hoverBackgroundColor: ["#FFB74D", "#81C784"],
+    }
+  ) {
+    let pending = 0,
+      resolved = 0;
+    data.forEach((report) => {
+      if (report.status === "completed") {
+        resolved += 1;
+      } else if (report.status === "pending") {
+        pending += 1;
+      }
+    });
+    return {
+      labels: ["Pending", "Resolved"],
+      datasets: [
+        {
+          data: [pending, resolved],
+          ...styles,
+        },
+      ],
+    };
+  }
 
-  const [lightOptions] = useState({
+  let chartData = generateStatistics(reportData);
+  const lightOptions = {
     plugins: {
       legend: {
         labels: {
@@ -22,7 +39,7 @@ const OrgReportStats = ({ reportData }) => {
         },
       },
     },
-  });
+  };
 
   return (
     <div className="main" style={{ backgroundColor: "white" }}>
@@ -55,13 +72,19 @@ const OrgReportStats = ({ reportData }) => {
                   className="block text-2xl  mb-1"
                   style={{ color: "orange" }}
                 >
-                  <strong>Pending :</strong> 30
+                  <strong>Pending :</strong> {chartData.datasets[0].data[0]}
                 </span>
-                <span className="block text-2xl  mb-1" style={{ color: "red" }}>
-                  <strong>Resolved :</strong> 30
+                <span
+                  className="block text-2xl  mb-1"
+                  style={{ color: "#66BB6A" }}
+                >
+                  <strong>Resolved :</strong> {chartData.datasets[0].data[1]}
                 </span>
                 <span className="block text-2xl mb-1">
-                  <strong>Number of active residents:</strong> 100+
+                  <strong>Number of active residents:</strong>{" "}
+                  {chartData.datasets[0].data[0] +
+                    chartData.datasets[0].data[1]}
+                  +
                 </span>
               </div>
             </div>
